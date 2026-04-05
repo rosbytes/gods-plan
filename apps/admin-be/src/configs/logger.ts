7952f33ad3af73d1a27bd288bb66d99c2bbc5e92
Bhawani Singh <bhawanibytes@gmail.com>
2026-04-06 01:18:38 +0530

    refactor: update logger configuration and regenerate application bundle

--- apps/admin-be/src/configs/logger.ts
+++ apps/admin-be/src/configs/logger.ts
@@ -1,53 +1,43 @@
 import { addColors, createLogger, format, transports } from "winston"
-// import { env } from "./env"
-//colors
+
+// colors
 const colors = {
     info: "blue",
     warn: "yellow",
     error: "red",
     debug: "gray",
-    deal: "black",
 }
 addColors(colors)
 
-//formats
+// formats
 const baseFormat = format.combine(
     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     format.splat(),
     format.metadata({ fillExcept: ["timestamp", "level", "message", "stack"] }),
 )
-// const consoleFormat = format.printf(({ timestamp, level, message, metadata }) => {
-//     const timeOnly = timestamp as string
-//     const metaString =
-//         metadata && Object.keys(metadata).length ? `${JSON.stringify(metadata)}` : " "
-//     return `${timeOnly} [${level}] ${message} ${metaString}`
-// })
-// const uppercaseFormat = format((info) => {
-//     info.level = info.level.toUpperCase()
-//     return info
-// })
+
+const isVercel = process.env.VERCEL === "1"
 
 const loggerTransports: (transports.FileTransportInstance | transports.ConsoleTransportInstance)[] =
-    [
-        new transports.File({
-            filename: "log/app.log",
-            format: format.json(),
-        }),
-    ]
-
-// if (env.NODE_ENV === "development") {
-//     loggerTransports.push(
-//         new transports.Console({
-//             format: format.combine(uppercaseFormat(), format.colorize(), consoleFormat),
-//         }),
-//     )
-// }
+    isVercel
+        ? [new transports.Console()]
+        : [
+              new transports.File({
+                  filename: "log/app.log",
+                  format: format.json(),
+              }),
+              new transports.Console(),
+          ]
 
 export const logger = createLogger({
     level: "info",
     format: baseFormat,
     transports: loggerTransports,
-    exceptionHandlers: [new transports.File({ filename: "log/exceptions.log" })],
-    rejectionHandlers: [new transports.File({ filename: "log/rejections.log" })],
+    exceptionHandlers: isVercel
+        ? [new transports.Console()]
+        : [new transports.File({ filename: "log/exceptions.log" })],
+    rejectionHandlers: isVercel
+        ? [new transports.Console()]
+        : [new transports.File({ filename: "log/rejections.log" })],
     exitOnError: false,
 })
