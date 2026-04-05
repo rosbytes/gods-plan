7f4cf7845bc8dedb014974edad392792537f3128
Bhawani Singh <bhawanibytes@gmail.com>
2026-04-05 23:36:54 +0530

    refactor: fix package.json dependency structure and add explicit express type imports to server.ts

--- apps/admin-be/src/server.ts
+++ apps/admin-be/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import { createExpressMiddleware } from "@trpc/server/adapters/express"
 import { appRouter, createContext } from "./trpc"
 import cors from "cors"
@@ -14,7 +14,7 @@ app.set("trust proxy", false)
 app.use(cors({ origin: [env.FRONTEND_URL, "http://localhost:5173"], credentials: true }))
 app.use(express.json())
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("Server is working")
 })
 
