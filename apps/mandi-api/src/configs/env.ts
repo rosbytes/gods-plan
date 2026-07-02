15c4b075ea0069b26376c8d7474ee1274c75bef9
Bhawani Singh <bhawanibytes@gmail.com>
2026-07-02 06:31:48 +0530

    feat: created the env and logger configs

---
+++ apps/mandi-api/src/configs/env.ts
@@ -0,0 +1,26 @@
+import "dotenv/config"
+import * as z from "zod"
+// Schema to parse Env Variable
+const envSchema = z.object({
+    SERVER_PORT: z.coerce.number().int().min(1).max(65535),
+
+    FRONTEND_URL: z.url(),
+    DATABASE_URL: z.url(),
+
+    NODE_ENV: z.enum(["development", "production"]),
+
+    VERCEL: z.enum(["true", "false"]).transform((value) => value === "true"),
+})
+
+const createEnv = (env: NodeJS.ProcessEnv) => {
+    // Parse Env
+    const result = envSchema.safeParse(env)
+    if (!result.success) {
+        console.error("Failed to validate Env:", result.error)
+        process.exit(1)
+    }
+    return result.data
+}
+
+// export safelyParsed env object
+export const env = createEnv(process.env)
---
+++ apps/mandi-api/src/configs/index.ts
@@ -0,0 +1,2 @@
+export { logger } from "./logger"
+export { env } from "./env"
