d90d562788c587ae8d98ac84a3786abf1262b2f2
Bhawani Singh <bhawanibytes@gmail.com>
2026-07-06 04:33:56 +0530

    feat: add TRPC setup and dependencies for improved API communication

---
+++ apps/mandi/src/libs/trpc.ts
@@ -0,0 +1,14 @@
+import { createTRPCReact } from "@trpc/react-query"
+import type { AppRouter } from "../../../mandi-api/src/trpc/appRouter"
+import { httpBatchLink } from "@trpc/client"
+
+export const trpc = createTRPCReact<AppRouter>()
+
+// Create the client
+export const trpcClient = trpc.createClient({
+    links: [
+        httpBatchLink({
+            url: `${import.meta.env.VITE_API_URL}/trpc`, // backend tRPC endpoint
+        }),
+    ],
+})
--- apps/mandi/src/main.tsx
+++ apps/mandi/src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { BrowserRouter } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { trpc, trpcClient } from "./libs/trpc.ts"
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <trpc.Provider client={trpcClient} queryClient={queryClient}>
+                <QueryClientProvider client={queryClient}>
+                    <App />
+                </QueryClientProvider>
+            </trpc.Provider>
         </BrowserRouter>
     </StrictMode>,
 )
