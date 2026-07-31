6b3cfa6c8cd8c7c8420fc8eb90d617d95fda9aac
Bhawani Singh <bhawanibytes@gmail.com>
2026-07-31 18:41:22 +0530

    refactor: remove Vercel ignore file and update tsup build configuration for cleaner output

--- apps/admin-api/tsup.config.ts
+++ apps/admin-api/tsup.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-    entry: {
-        bundle: "src/app.ts",
-    },
+    entry: ["src/app.ts"],
     format: ["cjs"],
     splitting: false,
     sourcemap: true,
-    clean: false,
+    clean: true,
     bundle: true,
     // noExternal bundles all the external lib code in the bundlepnpm
     noExternal: [/.*/],
-    outDir: "./dist",
+    // outDir: "./dist",
     outExtension: () => ({ js: ".cjs" }),
 })
