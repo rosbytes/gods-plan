012cf109a480d2095a595f6548bc8c156b89e186
Bhawani Singh <bhawanibytes@gmail.com>
2026-08-14 03:26:25 +0530

    fix(mandi): fix auth middleware, trpc client, and protected route handling

--- apps/mandi/src/libs/trpc.ts
+++ apps/mandi/src/libs/trpc.ts
@@ -1,6 +1,7 @@
 import { createTRPCReact } from "@trpc/react-query"
 import type { AppRouter } from "../../../mandi-api/src/trpc/appRouter"
 import { httpBatchLink } from "@trpc/client"
+import { customFetch } from "./customFetch"
 
 export const trpc = createTRPCReact<AppRouter>()
 
@@ -9,24 +10,7 @@ export const trpcClient = trpc.createClient({
     links: [
         httpBatchLink({
             url: `${import.meta.env.VITE_API_URL}/trpc`, // backend tRPC endpoint
-            headers() {
-                try {
-                    // TODO: better accept the cookies
-                    const storeData = localStorage.getItem("mandi-store")
-                    if (storeData) {
-                        const parsed = JSON.parse(storeData)
-                        const token = parsed.state?.user?.token
-                        if (token) {
-                            return {
-                                Authorization: `Bearer ${token}`,
-                            }
-                        }
-                    }
-                } catch (e) {
-                    console.error("Failed to parse auth token for headers", e)
-                }
-                return {}
-            },
+            fetch: customFetch,
         }),
     ],
 })
--- apps/mandi/src/main.tsx
+++ apps/mandi/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx"
 import { BrowserRouter } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { trpc, trpcClient } from "./libs/trpc.ts"
+import { Toaster } from "sonner"
 
 const queryClient = new QueryClient()
 
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
             <trpc.Provider client={trpcClient} queryClient={queryClient}>
                 <QueryClientProvider client={queryClient}>
                     <App />
+                    <Toaster richColors position="top-center" />
                 </QueryClientProvider>
             </trpc.Provider>
         </BrowserRouter>
