6170780ae6edac8e95f9b26562cc5e56b8a4602c
Bhawani Singh <bhawanibytes@gmail.com>
2026-08-13 17:16:11 +0530

    solving 500 in vercel

--- apps/admin-api/tsup.config.ts
+++ apps/admin-api/tsup.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     clean: true,
     bundle: true,
     // noExternal: [/.*/],
-    noExternal: ["@ros/db", "@ros/commons"],
+    noExternal: ["@ros/db", "@ros/ui", "@ros/commons"],
     outExtension: () => ({ js: ".cjs" }),
 })
