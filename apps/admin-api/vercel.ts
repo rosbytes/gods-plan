f7bb4746131c237cb85dcf71d1f828ed6ed767d5
Bhawani Singh <bhawanibytes@gmail.com>
2026-08-13 18:40:00 +0530

    handled with routes instead of rewrite

--- apps/admin-api/vercel.ts
+++ apps/admin-api/vercel.ts
@@ -2,5 +2,11 @@ import { type VercelConfig, routes } from "@vercel/config/v1"
 
 export const config: VercelConfig = {
     // framework: "express",
-    rewrites: [routes.rewrite("/(.*)", "/api")],
+    // rewrites: [routes.rewrite("/(.*)", "/api")],
+    routes: [
+        {
+            src: "/(.*)",
+            dest: "/api",
+        },
+    ],
 }
