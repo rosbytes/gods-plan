28828140ad5e1b6ac735fcac46295b65866826ca
Bhawani Singh <bhawanibytes@gmail.com>
2026-08-13 18:30:42 +0530

    commented the framework

--- apps/admin-api/vercel.ts
+++ apps/admin-api/vercel.ts
@@ -1,6 +1,6 @@
 import { type VercelConfig, routes } from "@vercel/config/v1"
 
 export const config: VercelConfig = {
-    framework: "express",
+    // framework: "express",
     rewrites: [routes.rewrite("/(.*)", "/api")],
 }
